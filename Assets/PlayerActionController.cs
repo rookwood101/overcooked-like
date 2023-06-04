5a939e2cf8f90bed9bfdc6bfc270804af2517b47
Alex Sadler <rookwood101@users.noreply.github.com>
2023-06-04 19:52:49 +0100

    Interact with stations to make a sword

--- Assets/PlayerActionController.cs
+++ Assets/PlayerActionController.cs
@@ -1,48 +1,162 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.Assertions;
 using UnityEngine.InputSystem;
 
 public class PlayerActionController : MonoBehaviour
 {
+    // constants
+    private static readonly Dictionary<string, string> unlimitedInventoryStations = new Dictionary<string, string>() {
+        {"stock_pile", "stock"}
+    };
+    private static readonly Dictionary<string, Dictionary<string, string>> transformationStations = new Dictionary<string, Dictionary<string, string>>() {
+        {"forge", new Dictionary<string, string>() {
+            {"stock", "hot_stock"}
+        }},
+        {"anvil", new Dictionary<string, string>() {
+            {"hot_stock", "hot_shaped_stock"}
+        }},
+        {"water_bath", new Dictionary<string, string>() {
+            {"hot_shaped_stock", "tempered_shaped_stock"}
+        }},
+        {"grind_stone", new Dictionary<string, string>() {
+            {"tempered_shaped_stock", "blade"}
+        }}
+    };
+
+    // semi state
     private HashSet<GameObject> collidingObjects = new HashSet<GameObject>();
+    
+    // state
+    private static Dictionary<GameObject, string> inventories = new Dictionary<GameObject, string>();
+    private static Dictionary<GameObject, TransformAttempt> stationTransformAttempts = new Dictionary<GameObject, TransformAttempt>();
 
-    private string inventory = null;
-
-    // have inventory per game object of interest
-    // should scrap be modeled as an infinite inventory?
     // think about using a state machine model
     // if we go that route we'll need someway to update the game to match the state
 
-    private void InventoryAdd(string item) {
-        Assert.IsNull(inventory);
-        inventory = item;
-        Debug.Log("Inventory: " + inventory);
+    private void SetInventory(GameObject inventory, string item) {
+        inventories[inventory] = item;
+        Debug.Log($"{inventory.tag}: {item}");
+
+        // Delete existing inventory item game object
+        Transform[] childTransforms = inventory.transform.GetComponentsInChildren<Transform>();
+        foreach (Transform childTransform in childTransforms)
+        {
+            if (childTransform.CompareTag("inventory_item"))
+            {
+                Destroy(childTransform.gameObject);
+            }
+        }
+
+        // Load the prefab for inventory item just above inventory game object
+        var loadedPrefab = Resources.Load<GameObject>(item);
+        if (loadedPrefab != null)
+        {
+            var instance = Instantiate(loadedPrefab, inventory.transform);
+            instance.transform.localPosition = Vector3.up;
+        }
     }
 
-    private void InventoryRemove(string item) {
-        Assert.AreEqual(item, inventory);
-        inventory = null;
-        Debug.Log("Inventory empty");
+    private string GetInventory(GameObject inventory) {
+        return inventories.GetValueOrDefault(inventory);
     }
 
     public void OnPickUpPutDownButton(InputAction.CallbackContext context) {
         if (context.action.triggered && collidingObjects.Count == 1 ) {
-            if (collidingObjects.Single().tag == "scrap") {
-                if (inventory == null) {
-                    InventoryAdd("scrap");
-                } else {
-                    InventoryRemove(inventory);
-                }
+            var station = collidingObjects.Single();
+            if (GetInventory(gameObject) == null) {
+                // the player is holding nothing. Pick up station inventory or station default
+                SetInventory(gameObject, GetInventory(station) ?? unlimitedInventoryStations.GetValueOrDefault(station.tag));
+                SetInventory(station, null);
+
+                CancelTransformingForStation(station);
+            } else if (GetInventory(station) == null) {
+                // the player is holding something. Put it down
+                SetInventory(station, GetInventory(gameObject));
+                SetInventory(gameObject, null);
+
+                CancelTransformingForStation(station);
+            } else {
+                // the player is holding something but there's nowhere to put it down
+                // TODO: sad sound effect?
             }
         }
     }
 
     public void OnActivateButton(InputAction.CallbackContext context) {
-        if (context.action.triggered && collidingObjects.Count == 1 && collidingObjects.Single().tag == "anvil") {
-            Debug.Log("Activated anvil");
+        if (collidingObjects.Count > 1) {
+            Debug.Log("Near too many stations, don't know which to activate");    
+            return;
+        }
+        if (collidingObjects.Count == 0) {
+            return;
+        }
+
+        var depressed = context.ReadValueAsButton();
+        var station = collidingObjects.Single();
+        if (depressed) {
+            var transformations = transformationStations.GetValueOrDefault(station.tag);
+            var stationInventory = GetInventory(station);
+            if (transformations != null && stationInventory != null) {
+                var transformation = transformations.GetValueOrDefault(stationInventory);
+                if (transformation != null) {
+                    StartTransforming(station, true);
+                }
+            }
+        } else {
+            CancelTransformingForPlayer(gameObject, station);
+        }
+    }
+
+    private async void StartTransforming(GameObject station, bool requiresPlayer) {
+        var from = GetInventory(station);
+        var to = transformationStations[station.tag][from];
+        var isAttemptCreator = !stationTransformAttempts.ContainsKey(station);
+        var attempt = stationTransformAttempts.GetValueOrDefault(station, new TransformAttempt(){
+            from = from,
+            to = to,
+            lastFrameTime = Time.time,
+            requiresPlayer = requiresPlayer,
+        });
+        stationTransformAttempts[station] = attempt;
+        attempt.players.Add(gameObject);
+        if (isAttemptCreator) {
+            Debug.Log($"Transforming {from} to {to} at {station.tag}");
+            // TODO: time dependant on job - currently fixed at 5 seconds
+            while (attempt.elapsed < 5) {
+                Debug.Log($"Transforming {100*(attempt.elapsed / 5f)}%...");
+                await Task.Yield(); // TODO: may have to use https://github.com/Cysharp/UniTask
+                attempt.elapsed += (Time.time - attempt.lastFrameTime) * attempt.players.Count; // more players makes it go faster
+                attempt.lastFrameTime = Time.time;
+                if (attempt.cancelled) {
+                    return;
+                }
+            }
+            Debug.Log("Transforming complete!");
+            stationTransformAttempts.Remove(station);
+            SetInventory(station, to);
+        }
+    }
+
+    private void CancelTransformingForPlayer(GameObject player, GameObject station) {
+        foreach (var (transformingStation, attempt) in stationTransformAttempts.ToList()) {
+            if (transformingStation == station && attempt.requiresPlayer) {
+                attempt.players.Remove(player);
+                if (attempt.players.Count == 0) {
+                    Debug.Log($"Transformation at {station.tag} cancelled");
+                    attempt.cancelled = true;
+                    stationTransformAttempts.Remove(station);
+                }
+            }
+        }
+    }
+
+    private void CancelTransformingForStation(GameObject station) {
+        if (stationTransformAttempts.GetValueOrDefault(station) != null) {
+            Debug.Log($"Transformation at {station.tag} cancelled");
+            stationTransformAttempts[station].cancelled = true;
+            stationTransformAttempts.Remove(station);
         }
     }
 
@@ -54,5 +168,24 @@ public class PlayerActionController : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         collidingObjects.Remove(other.gameObject);
+        CancelTransformingForPlayer(player: gameObject, station: other.gameObject);
+    }
+}
+
+public record TransformAttempt {
+    public HashSet<GameObject> players {get;} = new HashSet<GameObject>();
+    public string from {get; init;}
+    public string to {get; init;}
+    public float lastFrameTime {get; set;}
+    public bool requiresPlayer {get; set;}
+    public float elapsed {get; set;} = 0;
+    public bool cancelled {get; set;} = false;
+}
+
+namespace System.Runtime.CompilerServices
+{
+    public class IsExternalInit
+    {
+
     }
 }
