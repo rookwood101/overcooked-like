8a9caa30074c2087a671322295dd93be4f2ea919
Alex Sadler <rookwood101@users.noreply.github.com>
2023-06-08 12:45:08 +0100

    Place items on tables and stations highlight when near

--- Assets/Triggerable.cs
+++ Assets/Triggerable.cs
@@ -1,11 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Triggerable : MonoBehaviour
 {
-    private float triggerScaleMultiplier = 1.4f; // Multiplier for the trigger's scale compared to the parent collider
+    private static readonly float triggerScaleMultiplier = 1.4f; // Multiplier for the trigger's scale compared to the parent collider
+    private static readonly Color highlightColor = new Color(0.2f, 0.2f, 0.2f, 1);
+
+    private HashSet<GameObject> collidingObjects = new HashSet<GameObject>();
+    private new Renderer renderer;
 
     private void Start()
     {
+        renderer = GetComponent<Renderer>();
+
         // Get the current game object's capsule collider
         CapsuleCollider parentCollider = GetComponent<CapsuleCollider>();
 
@@ -24,5 +31,24 @@ public class Triggerable : MonoBehaviour
         // Set the trigger collider's size to be larger
         triggerCollider.radius = parentCollider.radius * triggerScaleMultiplier;
         triggerCollider.height = parentCollider.height * triggerScaleMultiplier;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (collidingObjects.Count == 0) {
+            // Highlight
+            renderer.material.EnableKeyword("_EMISSION");
+            renderer.material.SetColor("_EmissionColor", highlightColor);
+        }
+        collidingObjects.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        collidingObjects.Remove(other.gameObject);
+        if (collidingObjects.Count == 0) {
+            // Unhighlight
+            renderer.material.DisableKeyword("_EMISSION");
+        }
     }
 }
